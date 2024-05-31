1e6cb01798056576478ea76f7e58a8e56791e1ba
NHJeans <guswo1013@naver.com>
2024-06-01 01:30:04 +0900

    Fix: login페이지 이름 변경 & router수정

--- src/apis/supabaseClient.js
+++ src/apis/supabaseClient.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase URL or key.");
