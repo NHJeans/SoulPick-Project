a3aecc5976f2c08fe7099564f0e7dc07b730384e
sorakim <sora6418@gmail.com>
2024-06-02 07:16:22 +0900

    Feat: add modal button component

---
+++ src/components/ModalButton/style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ModalButtonContainer = styled.button`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  right: 40px;
+  bottom: 60px;
+  width: 60px;
+  height: 60px;
+  background-color: #A0D2CF;
+  color: #000;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  
+`;
