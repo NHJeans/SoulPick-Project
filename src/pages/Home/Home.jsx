ddae512a3f1d61dd9e8a3d646a560eb81b30db12
sorakim <sora6418@gmail.com>
2024-06-02 08:04:31 +0900

    Fix : 모달버튼 추가 및 버튼 스타일 추가

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,28 @@
-import {HomeStyled} from "./style";
+import {HomeContainer} from "./style";
+import { Category } from "../../components/Category/index.js";
+import LayoutContainer from "../../components/Layout/LayoutContainer/index.js";
+import {useModal} from "../../components/Modal/ModalProvider/ModalProvider.jsx";
+import ModalButton from "../../components/ModalButton/index.js";
+import ContentsList from "../../components/Contents/components/ContentsList/index.js";
+import Contents from "../../components/Contents/index.js";
 
 function Home() {
-  return <HomeStyled>home</HomeStyled>
+  const { openModal } = useModal();
+
+  const handleOpenModal = () => {
+    openModal();
+  };
+
+  return (
+    <HomeContainer>
+      <Category/>
+      <LayoutContainer>
+        <Contents />
+        <ModalButton onClick={handleOpenModal}/>
+      </LayoutContainer>
+    </HomeContainer>
+  );
 }
+
 export default Home;
+
