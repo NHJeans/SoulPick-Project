f2903c845f41991750080bc4e954a138a873d4b3
sorakim <sora6418@gmail.com>
2024-06-02 06:29:34 +0900

    Feat: layoutContainer 추가 및 공통 스타일 적용

---
+++ src/components/Layout/LayoutContainer/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 60px;
+  margin: 0 auto;
+  max-width: 1780px;
+`
