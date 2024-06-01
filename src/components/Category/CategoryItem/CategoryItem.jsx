b0a29da40adb0405b95ef6a482f4b6774a750368
sorakim <sora6418@gmail.com>
2024-06-02 06:28:24 +0900

    Feat: category 영역 추가 및 스타일 적용

---
+++ src/components/Category/CategoryItem/CategoryItem.jsx
@@ -0,0 +1,16 @@
+import {CategoryItem, CategoryItemContainer} from "./style.js";
+
+function CategoryItemList() {
+  return (
+        <CategoryItemContainer>
+          <CategoryItem>123</CategoryItem>
+          <CategoryItem>123</CategoryItem>
+          <CategoryItem>123</CategoryItem>
+          <CategoryItem>123</CategoryItem>
+          <CategoryItem>123</CategoryItem>
+          <CategoryItem>123</CategoryItem>
+          <CategoryItem>123</CategoryItem>
+        </CategoryItemContainer>
+    );
+}
+export default CategoryItemList;
---
+++ src/components/Category/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CategoryContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  margin-top: 120px;
+  padding: 80px 0;
+  width: 100%;
+  background: #F6FAF9;
+`;
