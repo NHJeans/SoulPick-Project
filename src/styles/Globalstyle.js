86cebe87525a24243d638dd808d5cd164d72775f
sorakim <sora6418@gmail.com>
2024-06-02 08:05:13 +0900

    Fix: GlobalStyle 수정 - font관련 스타일 추가 및 수정 필요

--- src/styles/Globalstyle.js
+++ src/styles/Globalstyle.js
@@ -4,8 +4,9 @@ import { reset } from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-  body {
-    font-family: "Noto Sans KR", sans-serif;
+  * {
+    // TODO: 수정 필요!
+    font-family: "Noto Sans KR", sans-serif !important; 
   }
 `;
 export default GlobalStyle
