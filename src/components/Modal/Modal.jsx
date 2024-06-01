ebae7187ed7c796e222f374dbea4f9d9b5e37d02
sorakim <sora6418@gmail.com>
2024-06-02 08:03:59 +0900

    Feat: 메인 페이지 모달 추가

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,23 @@
+import {useModal} from "./ModalProvider/ModalProvider.jsx";
+import {Container, ContentInner, ModalContainer, ModalContent, ScreenDim} from "./style.js";
+
+
+function ModalLayout({ children }){
+  const { closeModal } = useModal();
+  return (
+    <Container>
+      <ModalContainer>
+        {/*<ButtonModalClose onClick={closeModal}>*/}
+        {/*</ButtonModalClose>*/}
+        <div>
+          <ModalContent>
+            <ContentInner>{children}</ContentInner>
+          </ModalContent>
+        </div>
+        <ScreenDim onClick={closeModal} />
+      </ModalContainer>
+    </Container>
+  )
+}
+export default ModalLayout;
+
