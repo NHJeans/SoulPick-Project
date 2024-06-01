1af380fd733034b9e06bbb5ffcbae4111164bc80
sorakim <sora6418@gmail.com>
2024-06-01 15:23:49 +0900

    Fix: Layout -> BaseLayout 컴포넌트로 수정

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,14 +3,14 @@ import Detail from '../pages/Detail';
 import Home from '../pages/Home';
 import MyPage from '../pages/MyPage';
 import SignUp from '../pages/SignUp';
-import Layout from "../components/Layout/BaseLayout/index.js";
 import AuthLayout from "../components/Layout/AuthLayout/index.js";
 import SignIn from '../pages/SignIn';
+import BaseLayout from "../components/Layout/BaseLayout/index.js";
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: <BaseLayout />,
     children: [
       {
         path: '',
