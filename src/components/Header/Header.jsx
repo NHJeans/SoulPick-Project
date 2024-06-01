5a73a77ce6b62cc8cad9702fcdad99daa088cab2
sorakim <sora6418@gmail.com>
2024-06-01 14:53:28 +0900

    Feat: Header 컴포넌트 추가

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import {HeaderStyled} from './style';
+import Nav from "../Nav/index.js";
+
+function Header() {
+  return (
+    <HeaderStyled>
+      <Nav/>
+    </HeaderStyled>
+  )
+}
+
+export default Header;
---
+++ src/components/Header/style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const HeaderStyled = styled.header`
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  height: 80px;
+  padding: 0 20px;
+  background-color: #fff;
+  box-shadow: 0 4px 4px 0 rgb(0 0 0 / 5%);
+`;
+export const Logo = styled.div`
+  width: 100px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+  //background: linear-gradient(180deg, rgba(176,212,246,1) 0%, rgba(237,138,138,1) 100%);
+  //background-clip: text;
+  //-webkit-background-clip: text;
+  //color: transparent;
+`;
