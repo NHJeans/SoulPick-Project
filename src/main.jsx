95aea6d68ac3f7335dc7151f88f13a89e48ec6a4
sorakim <sora6418@gmail.com>
2024-06-02 12:51:59 +0900

    Fix: 모달 관련 파일 주석 처리

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./redux/config/configStore";
-import {ModalProvider} from "./components/Modal/ModalProvider/ModalProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <ModalProvider>
       <App />
-    </ModalProvider>
   </Provider>
 );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,22 @@
 import {HomeContainer} from "./style";
 import { Category } from "../../components/Category/index.js";
 import LayoutContainer from "../../components/Layout/LayoutContainer/index.js";
-import {useModal} from "../../components/Modal/ModalProvider/ModalProvider.jsx";
 import ModalButton from "../../components/ModalButton/index.js";
-import ContentsList from "../../components/Contents/components/ContentsList/index.js";
 import Contents from "../../components/Contents/index.js";
 
 function Home() {
-  const { openModal } = useModal();
-
-  const handleOpenModal = () => {
-    openModal();
-  };
+  // const { openModal } = useModal();
+  //
+  // const handleOpenModal = () => {
+  //   openModal();
+  // };
 
   return (
     <HomeContainer>
       <Category/>
       <LayoutContainer>
         <Contents />
-        <ModalButton onClick={handleOpenModal}/>
+        <ModalButton/>
       </LayoutContainer>
     </HomeContainer>
   );
