e2e3cf8446b17b005541b4fbb628fd4a2a4dd799
sorakim <sora6418@gmail.com>
2024-06-04 09:27:25 +0900

    Fix: 모달팝업 Ui , 기능 작업 수정

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -5,3 +5,10 @@ export const HomeContainer = styled.div`
   width: 100%;
 `
 
+export const HomeTitle = styled.h1`
+  margin: 120px auto 40px;
+  width: 1100px;
+  font-size: 32px;
+  font-weight: bold;
+`;
+
