3c8ef522d81a0b5b163ee4e6974fb0174bdd202e
sorakim <sora6418@gmail.com>
2024-06-04 09:23:33 +0900

    Feat: add modalButton Component

--- src/components/Contents/components/ContentList/ContentList.jsx
+++ src/components/Contents/components/ContentList/ContentList.jsx
@@ -1,9 +1,12 @@
-import {ContentItem, ContentsListContainer} from "./style.js";
+import {
+  ContentsListContainer,
+} from "./style.js";
+import ContentItem from "../ContentItem/index.js";
 
 
 const CONTENTS = [
   {name : '동방신기 오빠들 컴백했어여!~!!!!', contents: '귀걸이 네 몸무게~~~ 노노우~~~~ It\'s about the way you are~~~~~'},
-  {name : '엔믹스,,,,,,,,미쳐', contents: '못잃어 꼭 들어보세요,,,,,,,,사랑해여 엔믹스!!!!!!!!!!!!!!!!!! '},
+  {name : '엔믹스,,,,,,,,미쳐', contents: '못잃어 꼭 들어보세요,,,,,,,,사랑해여 엔믹스!!!!!!!!!!!!!!!!!! 사랑해여 엔믹스!!!!!!!!!!!!!!!!!!'},
   {name : '에스파 수파노바 중독 되버렸는걸,,, 나 좀 살려@,@', contents: '수수수수파노바 노바~ 난 몰랑'},
   {name : 'test1', contents: 'test'},
   {name : 'test2', contents: 'test'},
@@ -13,10 +16,7 @@ function ContentsList() {
   return (
     <ContentsListContainer>
       {CONTENTS.map((content) => (
-        <ContentItem key={content.name}>
-          <div>{content.name}</div>
-          <div>{content.contents}</div>
-        </ContentItem>
+        <ContentItem key={content.name} content={content}/>
       ))}
     </ContentsListContainer>
   )
--- src/components/Contents/components/ContentList/style.js
+++ src/components/Contents/components/ContentList/style.js
@@ -6,11 +6,3 @@ export const ContentsListContainer = styled.ul`
   justify-content: space-between;
   gap: 20px;
 `;
-export const ContentItem = styled.li`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  padding: 20px 40px;
-  border-radius: 16px;
-  border: 2px solid #ddd;
-`
