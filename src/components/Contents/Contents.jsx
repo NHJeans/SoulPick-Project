e0203979eced7f700b5cb6fe7e6cf728f18d23ab
sorakim <sora6418@gmail.com>
2024-06-06 06:22:25 +0900

    Fix: 컨텐츠 없을 경우  예외처리

--- src/components/Contents/Contents.jsx
+++ src/components/Contents/Contents.jsx
@@ -1,4 +1,4 @@
-import {ContentsContainer, ContentTitle} from "./style.js";
+import {ContentsContainer, ContentTitle, DefaultContents} from "./style.js";
 import ContentList from "./components/ContentList/index.js";
 import usePostsData from "../../hooks/Data/index.js";
 
@@ -14,8 +14,16 @@ function Contents({title}) {
   return (
     <ContentsContainer>
       <ContentTitle>{title || "추천합니다"}</ContentTitle>
-      <ContentList data={filteredData} />
+      {/*<ContentList data={filteredData} />*/}
+      {filteredData.length > 0 ? (
+        <ContentList data={filteredData} />
+      ) : (
+        <DefaultContents>
+          <p>해당 카테고리에 게시글이 없습니다.</p>
+        </DefaultContents>
+      )}
     </ContentsContainer>
+
   );
 }
 export default Contents;
