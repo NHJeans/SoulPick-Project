0cd76143ba768306f46bfa9376ce4ce33433d670
Leeseayoung <tnfusrltk12@naver.com>
2024-06-05 19:45:55 +0900

    Feat: 내가 쓴 글, 유저 정보 컴포넌트 분리!

---
+++ src/components/Information/MyPost/style.js
@@ -0,0 +1,39 @@
+export const styles = {
+  //  내가 쓴 글-------------------
+  borderOne: {
+    borderTop: '2px solid black',
+    margin: '0 0 20px'
+  },
+  borderTwo: {
+    borderTop: '2px solid black',
+    margin: '30px 0 0',
+    width: '800px',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  centerStyle: {
+    display: 'flex',
+    textalign: 'center',
+    margin: '0 auto',
+    width: 'fit-content',
+    marginTop: '-80px',
+    width: '800px'
+  },
+  textTop: {
+    fontSize: '30px',
+    margin: ' 0 0 25px '
+  },
+
+  textSecond: {
+    fontSize: '25px',
+    margin: ' 0 0 10px '
+  },
+
+  imgStyle: {
+    height: '80px',
+    width: '80px',
+    float: 'right',
+    marginTop: '60px'
+  }
+};
