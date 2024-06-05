df3607dcfb90700af36c7d45b0fd6f6035f09f33
sorakim <sora6418@gmail.com>
2024-06-05 18:08:48 +0900

    Feat: 모달에서 데이터 입력 로직 생성

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,10 +7,32 @@ import {
   ModalTitle,
   ScreenDim
 } from "./style.js";
+import { useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import supabase from "../../apis/supabaseClient.js";
 
+function ModalLayout({ closeModal }) {
+  const [title, setTitle] = useState('');
+  const [link, setLink] = useState('');
+  const [content, setContent] = useState('');
 
-function ModalLayout({closeModal }){
-  // const { closeModal } = useModal();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { data, error } = await supabase
+      .from('Posts')
+      .insert([{ title, link, content, category: 'ost'}]);
+
+    if (error) {
+      console.error('데이터 삽입 오류,,,ㅠㅠ:', error);
+    } else {
+      console.log('데이터가 성공적으로 입력완!!!', data);
+      setTitle('');
+      setLink('');
+      setContent('');
+      closeModal();
+    }
+  };
 
   return (
     <Container>
@@ -19,11 +41,26 @@ function ModalLayout({closeModal }){
           <ModalContent>
             <ContentInner>
               <ModalTitle>my Pick!</ModalTitle>
-              <ModalForm>
-                <ModalInput type="text" placeholder="제목을 입력해주세요." />
-                <ModalInput type="text" placeholder="링크를 입력해주세요." />
-                <ModalTextarea type="text" placeholder="내용을 입력해주세요." />
-                <ModalSubmitButton type={'submit'}>등록하기</ModalSubmitButton>
+              <ModalForm onSubmit={handleSubmit}>
+                <ModalInput
+                  type="text"
+                  placeholder="제목을 입력해주세요."
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <ModalInput
+                  type="text"
+                  placeholder="링크를 입력해주세요."
+                  value={link}
+                  onChange={(e) => setLink(e.target.value)}
+                />
+                <ModalTextarea
+                  type="text"
+                  placeholder="내용을 입력해주세요."
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                />
+                <ModalSubmitButton type="submit">등록하기</ModalSubmitButton>
               </ModalForm>
             </ContentInner>
           </ModalContent>
@@ -31,7 +68,7 @@ function ModalLayout({closeModal }){
         <ScreenDim onClick={closeModal} />
       </ModalContainer>
     </Container>
-  )
+  );
 }
-export default ModalLayout;
 
+export default ModalLayout;
