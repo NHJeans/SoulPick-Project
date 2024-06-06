8f16492399713311c67fc996b7dc79bc20559768
NHJeans <guswo1013@naver.com>
2024-06-07 01:07:02 +0900

    Fix : configStore import문 충돌 해결

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -2,8 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import commentSlice from '../slices/commentSlice';
 import userSlice from '../slices/userSlice';
 
-import userSlice from '../slices/userSlice';
-
 // store 설정
 const store = configureStore({
   reducer: {
