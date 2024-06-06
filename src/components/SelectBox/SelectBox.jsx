0fe118334254d3b13316bcaddfa1779e2e97c086
sorakim <sora6418@gmail.com>
2024-06-06 21:47:03 +0900

    Fix: 메인 셀렉트 박스 로그인 로그아웃 기능별 로직 변경

--- src/components/SelectBox/SelectBox.jsx
+++ src/components/SelectBox/SelectBox.jsx
@@ -1,23 +1,34 @@
 import {SelectBoxListContainer, SelectBoxListItem} from "./style.js";
 import {useNavigate} from "react-router-dom";
 
-const OPTIONS = [
-  { value: 'signUp', label: '회원가입', path: '/auth/signup'},
-  { value: 'logIn', label: '로그인' , path: '/auth/signin'},
-  { value: 'myPage', label: '마이페이지', path: '/mypage' },
-  { value: 'logOut', label: '로그아웃' , path: '/'},
+const OPTIONS_LOGGED_OUT = [
+  { value: 'signUp', label: '회원가입', path: '/auth/signup' },
+  { value: 'logIn', label: '로그인', path: '/auth/signin' },
 ];
-function SelectBox({onClose}) {
-    const navigagte = useNavigate();
+
+const OPTIONS_LOGGED_IN = [
+  { value: 'myPage', label: '마이페이지', path: '/mypage' },
+  { value: 'logOut', label: '로그아웃', path: '/' },
+];
+
+function SelectBox({ isLoggedIn, onClose, onLogout }) {
+  const navigate = useNavigate();
 
   const handleSelect = (path) => {
-    navigagte(path);
-    onClose();
-  };
+    if (path === '/') {
+      onLogout();
+    } else {
+      navigate(path);
+      onClose();
+    }
+  }
+
+
+  const options = isLoggedIn ? OPTIONS_LOGGED_IN : OPTIONS_LOGGED_OUT;
 
   return (
     <SelectBoxListContainer>
-      {OPTIONS.map((option) => (
+      {options.map((option) => (
         <SelectBoxListItem key={option.value} onClick={() => handleSelect(option.path)}>
           {option.label}
         </SelectBoxListItem>
